2d223294d580fc879e659f4147b7aacc570bef33
Ahmet5300 <25asahin25@gmail.com>
2017-09-05 14:47:09 +0200

    video 6
    Als developer wil ik index.php zien, zodat ik weet dat ik vooruitgang
    boek.

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
-
-echo 'Hello.';
-
+$name = 'Ahmet';
+echo $name. "<br>";
+$daysInWeek =7;
+echo $daysInWeek. "<br>";
+echo 'There is a ' . $daysInWeek . ' in a week.' . "<br>";
+$text = 'There is a ' . $daysInWeek . ' in a week.' . "<br>";
+echo $text;
 ?>
