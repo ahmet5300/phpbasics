7d468d222687c7d96d36076203e4a67215c5c9d9
Ahmet5300 <25asahin25@gmail.com>
2017-09-06 10:33:40 +0200

    video 25: Functions flexibility
    als developer wil ik meer te weten te komen over functions, zodat ik
    deze functie vaker kan toepassen.

--- functions.php
+++ functions.php
@@ -10,12 +10,27 @@
 //}
 //
 //$fullName = fullName();
-//echo $fullName;
+////echo $fullName;
+//
+//function firtName($first, $second){
+//	return "the firstname is " . ': ' . $first . ' and the second name is ' . ': ' . $second;
+//}
+//$fullname = firtName('Ahmet', 'Kees');
+//
+//echo $fullname;
 
-function firtName($first, $second){
-	return "the firstname is " . ': ' . $first . ' and the second name is ' . ': ' . $second;
-}
-$fullname = firtName('Ahmet', 'Kees');
+function fullName($firstName, $lastName=null, $seperator = '_'){
+	
+	if(trim($firstName) === '' ){
+		return;
+	}
+	
+	if($lastName === null){
+		
+		return 'Ahmet';
+	}
 
-echo $fullname;
+	return "{$firstName}{$seperator}{$lastName}";
+}  
+echo fullName('Ahmet', 'Sahin');
 ?>
