e0c1a0d033a4f43922931f1565b5f0b8b0f6e7a1
Ahmet5300 <25asahin25@gmail.com>
2017-09-06 10:07:50 +0200

    video 24: Functions basics
    Als developer wil ik meer weten over function(basis), zodat ik dit kan
    gebruiken indien het nodig is.

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+//function fullName(){
+//	echo "Ahmet is 25";
+//}
+//fullName();
+
+//function fullName(){
+//	return "Ahmet is 25";
+//}
+//
+//$fullName = fullName();
+//echo $fullName;
+
+function firtName($first, $second){
+	return "the firstname is " . ': ' . $first . ' and the second name is ' . ': ' . $second;
+}
+$fullname = firtName('Ahmet', 'Kees');
+
+echo $fullname;
+?>
