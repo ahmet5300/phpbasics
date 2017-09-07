64bb811a989d5d051903af11398296dc43b83fb3
Ahmet5300 <25asahin25@gmail.com>
2017-09-07 10:41:20 +0200

    video 32: Inside functions
    als developer wil ik de ingebouwde functies beheersen, zodat ik ze vaker
    kan gebruiken.

---
+++ insidefunctions.php
@@ -0,0 +1,24 @@
+<?php
+
+//echo strlen('Ahmet') . "<br>";
+//
+//$name='hooii';
+//$name1 = "   haaai";
+//echo strtoupper($name) . "<br>";
+//echo strtolower($name) . "<br>";
+//echo trim($name1) . "<br>";
+////echo substr($name1, 0,30);
+//
+//$page=$_GET['page']?? 1;
+//
+//if(empty(trim($page))){
+//	$page = 1;
+//}
+////echo $page;
+//
+//$number= 1000030030303.00988;
+//
+//echo number_format($number, 2,'.','');
+
+header('Location: postenget.php');
+?>
--- postenget.php
+++ postenget.php
@@ -23,7 +23,7 @@
 		<form action=signin.php method="post">
 			<label for="username">Username</label>
 			<input type="text" name="username" id="username">
-			<label for="password">Passsssword</label>
+			<label for="password">Password</label>
 			<input type="password" name="password" id="password">
 			
 			<button type="submit">Sign in</button>
